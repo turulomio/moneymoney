c2504d1f1da4ac18b9217c90788046d65b0c3d97
turulomio <turulomio@yahoo.es>
2021-09-22 12:40:25 +0200

    Fix bug selecting a filename. Fixes #46

--- src/components/ProductsUpdate.vue
+++ src/components/ProductsUpdate.vue
@@ -1,18 +1,16 @@
 <template>
-    <div>
+    <div class="pa-6">
         <h1>{{ $t(`Products update`)}}</h1>
- 
-        <v-form ref="formFile" method="post">
-            <v-card>
-                <v-file-input name="csv_file1" accept="text/csv" show-size truncate-length="100" v-model="file" :label="$t('Select a file')"></v-file-input>
-                <v-card-actions class="justify-center">
-                    <v-btn color="primary" @click="submmit()" :disabled="loading==true">{{ $t("Send file")}}</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-form>
+        <v-card class="pa-5">
+            <v-form ref="formFile" method="post">
+                    <v-file-input name="csv_file1" accept="text/csv" show-size truncate-length="100" v-model="file" :label="$t('Select a file')"></v-file-input>
+                    <v-card-actions class="justify-center">
+                        <v-btn color="primary" @click="submmit()" :disabled="isButtonDisabled()">{{ $t("Send file")}}</v-btn>
+                    </v-card-actions>
+            </v-form>
+        </v-card>   
 
-        <v-data-table dense :headers="headers" :items="items" sort-by="name" class="elevation-1" hide-default-footer  disable-pagination :loading="loading">
-        </v-data-table>
+        <v-data-table dense :headers="headers" :items="items" sort-by="name" class="elevation-1 pt-6" hide-default-footer  disable-pagination :loading="loading"></v-data-table>
     </div>
 </template>  
 <script>     
@@ -49,7 +47,13 @@
                 }, (error) => {
                     this.parseResponseError(error)
                 })
+            },
+            isButtonDisabled(){
+                if (this.loading==true) return true
+                if (this.file=="") return true
+                return false
             }
+
         }
         
     }
