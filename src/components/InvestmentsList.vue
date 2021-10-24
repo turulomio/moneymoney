faba81cf4317da067d9e8c9645b13784d8ff2996
turulomio <turulomio@yahoo.es>
2021-10-24 09:15:42 +0200

    Ordering inactive investments by name

--- src/components/InvestmentsList.vue
+++ src/components/InvestmentsList.vue
@@ -8,7 +8,7 @@
                 <v-checkbox class="ml-6 mr-10" v-model="showActive" :label="setCheckboxLabel()" @click="on_chkActive()" ></v-checkbox>
                 <v-text-field class="ml-10 mr-6" v-model="search" append-icon="mdi-magnify" :label="$t('Filter')" single-line hide-details :placeholder="$t('Add a string to filter table')"></v-text-field>
             </v-row>
-            <v-data-table dense :headers="investments_headers" :search="search" :items="investments_items" sort-by="percentage_selling_point" class="elevation-1 ma-4" hide-default-footer disable-pagination :loading="loading_investments" :key="key">
+            <v-data-table dense :headers="investments_headers" :search="search" :items="investments_items" :sort-by="(showActive)? 'percentage_selling_point': 'fullname' " class="elevation-1 ma-4" hide-default-footer disable-pagination :loading="loading_investments" :key="key">
                 <template v-slot:[`item.fullname`]="{ item }">
                     {{item.fullname}}
                 </template>                  
