d32846ff2b4c1838dfa891eaa91c2fd5fecd2367
turulomio <turulomio@yahoo.es>
2022-04-15 10:47:42 +0200

    Changed default color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,6 +5,16 @@ import en from 'vuetify/src/locale/en.ts'
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+      themes: {
+        light: {
+          primary: '#546e7a',
+          secondary: '#b0bec5',
+          accent: '#8c9eff',
+          error: '#b71c1c',
+        },
+      },
+    },
     lang: {
         locales: {es, en },
         current: 'en'
