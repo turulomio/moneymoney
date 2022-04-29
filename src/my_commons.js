23de5e8d435df8b3033d66d0732713c669b769d7
turulomio <turulomio@yahoo.es>
2022-04-29 18:49:36 +0200

    localtime guess browser time zone

--- src/my_commons.js
+++ src/my_commons.js
@@ -23,7 +23,7 @@ export function localtime(value){
     if (value){
         var dateFormat = 'YYYY-MM-DD HH:mm:ss';
         var testDateUtc = moment.utc(value);
-        var localDate = testDateUtc.tz(this.$store.state.local_zone)
+        var localDate = testDateUtc.tz(Intl.DateTimeFormat().resolvedOptions().timeZone)
         return (localDate.format(dateFormat)); // 2015-30-01 02:00:00
     }
     return null;
