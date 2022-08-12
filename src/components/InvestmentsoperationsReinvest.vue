42707648335e4d3ee51ebcce73ec4ef1343ff6eb
turulomio <turulomio@yahoo.es>
2022-08-12 13:30:21 +0200

    Added disinvest feature

--- src/components/InvestmentsoperationsReinvest.vue
+++ src/components/InvestmentsoperationsReinvest.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>{{ $t(`Reinvest dialog`)}}
+        <h1>{{ $t(`Reinvest / Disinvest dialog`)}}
             <MyMenuInline :items="items" :context="this"></MyMenuInline>
         </h1>
         <v-card class="pa-4 mb-3 mt-3"  >
@@ -14,10 +14,10 @@
                 </v-row>
 
             <v-row>
-                <v-select class="mr-5" :disabled="loading" :items="viewoptions" v-model="viewoption" :label="$t('Set a view option')"  item-text="name" item-value="id" :rules="RulesSelection(true)" @change="refreshTables()"></v-select>  
-                <v-text-field class="mr-5" autoindex="1" :disabled="loading" v-model="gains_percentage" type="number" :label="$t('Gains percentage')" :placeholder="$t('Gains percentage')" :rules="RulesFloat(5,true)" counter="5"/>
+                <v-select class="mr-5" :disabled="loading || (ios_after==null)" :items="viewoptions" v-model="viewoption" :label="$t('Set a view option')"  item-text="name" item-value="id" :rules="RulesSelection(true)" @change="refreshTables()"></v-select>  
+                <v-text-field class="mr-5" autoindex="1" :disabled="loading" v-model="gains_percentage" type="number" :label="$t('Gains percentage')" :placeholder="$t('Gains percentage')" :rules="RulesFloat(8,true)" counter="8"/>
 
-                <v-btn class="mr-5" color="primary" @click="make_all_axios()" :disabled="!form_valid">{{ $t("Simulate") }}</v-btn>
+                <v-btn class="mr-5" color="primary" @click="make_all_axios_after()" :disabled="!form_valid">{{ $t("Simulate") }}</v-btn>
                 <v-btn v-if="this.newinvestments.length==1" color="error" @click="add_order()" :disabled="!form_valid">{{ $t("Add order") }}</v-btn>                 
             </v-row>
             </v-form>  
@@ -109,18 +109,95 @@
                         subheader:this.$t('Options to set shares'),
                         children: [
                             {
-                                name:this.$t('Integer shares from price'),
+                                name:this.$t('Integer shares from amount to reinvest'),
                                 code: function(this_){
-                                    var amount=this_.parseNumber(prompt( this_.$t("Please the amount to invest in this order"), 10000 ));
+                                    var amount=this_.parseNumber(prompt( this_.$t("Please set the amount to invest in this order"), 10000 ));
                                     this_.newshares=parseInt(amount/this_.newprice)
                                 },
                                 icon: "mdi-book-plus",
                             },
                             {
-                                name:this.$t('Decimal shares from price'),
+                                name:this.$t('Decimal shares from amount to reinvest'),
                                 code: function(this_){
-                                    var amount=this_.parseNumber(prompt( this_.$t("Please the amount to invest in this order"), 10000 ));
+                                    var amount=this_.parseNumber(prompt( this_.$t("Please set the amount to invest in this order"), 10000 ));
                                     this_.newshares=amount/this_.newprice
+                                },
+                                icon: "mdi-book-plus",
+                            },
+                        ]
+                    },
+                    {
+                        subheader:this.$t('Options to set gains percentage to reinvest'),
+                        children: [
+                            {
+                                name:this.$t('Gains percentage from gains amount'),
+                                code: function(this_){
+                                    var invested=0
+                                    this_.ios_before.io_current.forEach(o=>{
+                                        invested=invested+o.invested_investment
+                                    })
+                                    console.log(invested)
+
+
+                                    var amount=this_.parseNumber(prompt( this_.$t("Please the amount to gain in this order"), 500 ));
+                                    this_.gains_percentage=this_.my_round(parseFloat(amount/invested)*100,4)
+                                },
+                                icon: "mdi-book-plus",
+                            },
+                        ]
+                    },
+                    {
+                        subheader:this.$t('Options to desinvest'),
+                        children: [
+                            {
+                                name:this.$t('Integer shares to consolidate losses'),
+                                code: function(this_){
+                                    var losses=this_.parseNumber(prompt( this_.$t("Please set losses to consolidate (Positive amount)"), 500 ));
+
+                                    var resultado=0
+                                    for (var i = 0; i < this_.ios_before.io_current.length; i++) {
+                                        var o=this_.ios_before.io_current[i]
+                                        var gains=o.gains_gross_investment
+                                        if (losses+gains==0){
+                                            resultado=resultado+o.shares
+                                            break
+                                        } else if (losses+gains>0){
+                                            resultado=resultado+o.shares
+                                            losses=losses+gains
+                                        } else if (losses+gains<0){
+                                            resultado=resultado+Math.abs(parseInt(losses*o.shares/gains))
+                                            break
+                                        }
+                                    }
+
+
+                                    this_.newshares=-resultado
+                                },
+                                icon: "mdi-book-plus",
+                            },
+                            {
+                                name:this.$t('Float shares to consolidate losses'),
+                                code: function(this_){
+                                    var losses=this_.parseNumber(prompt( this_.$t("Please set losses to consolidate (Positive amount)"), 500 ));
+
+                                    var resultado=0
+                                    for (var i = 0; i < this_.ios_before.io_current.length; i++) {
+                                        var o=this_.ios_before.io_current[i]
+                                        var gains=o.gains_gross_investment
+                                        if (losses+gains==0){
+                                            resultado=resultado+o.shares
+                                            break
+                                        } else if (losses+gains>0){
+                                            resultado=resultado+o.shares
+                                            losses=losses+gains
+                                        } else if (losses+gains<0){
+                                            resultado=resultado+Math.abs(losses*o.shares/gains)
+                                            break
+                                        }
+                                    }
+
+
+                                    this_.newshares=-this_.my_round(resultado,6)
                                 },
                                 icon: "mdi-book-plus",
                             },
@@ -132,7 +209,7 @@
                     {id:1, name:'Before simulation'},
                     {id:2, name:'After simulation'},
                 ],
-                viewoption:2,
+                viewoption:1,
                 list_io_current:[],
                 list_io:[],
                 list_io_historical:[],
@@ -200,13 +277,24 @@
                 operation.investments=this.investments[0]
                 simulation.operations.push(operation)
                 return axios.post(`${this.$store.state.apiroot}/investmentsoperations/full/simulation/`, simulation, this.myheaders())
+                
             },
-            make_all_axios(){
+            make_all_axios_before(){
                 this.loading=true
-                axios.all([this.refreshProductQuotes(), this.simulateOrderBefore(), this.simulateOrderAfter()])
-                .then(([resQuotes, resBefore, resAfter]) => {
+                axios.all([this.refreshProductQuotes(), this.simulateOrderBefore()])
+                .then(([resQuotes, resBefore]) => {
                     this.ohcls=resQuotes.data 
                     this.ios_before=resBefore.data
+                    console.log(this.ios_before)
+                    this.loading=false
+                    this.refreshTables()
+                });
+            },
+            make_all_axios_after(){
+                this.viewoption=2
+                this.loading=true
+                axios.all([this.simulateOrderAfter()])
+                .then(([resAfter]) => {
                     this.ios_after=resAfter.data
                     this.loading=false
                     this.refreshTables()
@@ -252,6 +340,7 @@
             if (this.newinvestments.length>0) {
                 this.product=this.$store.getters.getObjectPropertyByUrl("investments", this.newinvestments[0],"products")
             }
+            this.make_all_axios_before()
         }
         
     }
