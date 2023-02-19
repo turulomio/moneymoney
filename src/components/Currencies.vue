6a67b8894a6f9e2da8a758c58bba8ec08cfeb3f9
turulomio <turulomio@yahoo.es>
2023-02-19 09:33:21 +0100

    Currencies full working

--- src/components/Currencies.vue
+++ src/components/Currencies.vue
@@ -5,19 +5,22 @@
             <v-data-table dense :headers="headers" :items="items" sort-by="from" class="elevation-1 cursorpointer" hide-default-footer disable-pagination :key="key">
                 <template v-slot:[`item.datetime`]="{ item }">
                     {{localtime(item.datetime)}}
-                </template>         
+                </template>     
+                <template v-slot:[`item.supported`]="{ item }">
+                    <v-icon small v-if="item.supported" >mdi-check-outline</v-icon>
+                </template>       
                 <template v-slot:[`item.actions`]="{ item }">
-                    <v-icon v-if="item.supported" small class="mr-2" @click="addItem(item)">mdi-plus</v-icon>
-                    <v-icon v-if="item.factor" small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-                    <v-icon v-if="item.factor" small @click="deleteItem(item)">mdi-delete</v-icon>
+                    <v-icon v-if="item.can_c" small class="mr-2" @click="addItem(item)">mdi-plus</v-icon>
+                    <v-icon v-if="item.can_rud" small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+                    <v-icon v-if="item.can_rud" small @click="deleteItem(item)">mdi-delete</v-icon>
                 </template>
             </v-data-table>
         </v-card>
 
         <!-- QUOTES CU -->
-        <v-dialog v-model="dialog_quotescu" width="35%">
+        <v-dialog v-model="dialog_quotescu" width="50%">
             <v-card class="pa-4">
-                <QuotesCU :quote="quote" :key="key" />
+                <QuotesCU :quote="quote" :mode="quote_mode" :key="key" @cruded="on_QuotesCU_cruded" />
             </v-card>
         </v-dialog>
     </div>
@@ -34,42 +37,59 @@
         data(){ 
             return{
                 headers: [
-                    { text: this.$t('From'), sortable: true, value: 'from', width:"21%"},
-                    { text: this.$t('To'), sortable: true, value: 'to', width:"21%"},
-                    { text: this.$t('Date and time'), sortable: true, value: 'datetime', width:"21%"},
-                    { text: this.$t('Factor'), value: 'factor',  width: "8%"},
-                    { text: this.$t('Actions'), value: 'actions', sortable: false , width: "7%"},
+                    { text: this.$t('From'), sortable: true, value: 'from'},
+                    { text: this.$t('To'), sortable: true, value: 'to'},
+                    { text: this.$t('Supported'), sortable: true, value: 'supported'},
+                    { text: this.$t('Date and time'), sortable: true, value: 'datetime'},
+                    { text: this.$t('Factor'), value: 'quote'},
+                    { text: this.$t('Actions'), value: 'actions', sortable: false , width: "10%"},
                 ],
                 items:[],
                 dialog_quotescu:false,
                 quote:null,
+                quote_mode: null,
                 key:0,
             }
         },
         methods: {
             empty_quote,
             editItem (item) {
-                this.account=item
-                this.account_deleting=false
+                this.quote= {
+                    url: item.quote_url,
+                    datetime: item.datetime,
+                    quote: item.quote,
+                    products: item.product_url,
+                }
+                this.quote_mode="U"
                 this.key=this.key+1
-                this.dialog=true
+                this.dialog_quotescu=true
             },
             addItem (item) {
+                this.quote= {
+                    url: null,
+                    datetime: new Date(),
+                    quote: null,
+                    products: item.product_url,
+                }
+                this.quote_mode="C"
                 this.key=this.key+1
-                this.account=item
-                this.dialog_view=true
+                this.dialog_quotescu=true
             },
             deleteItem (item) {
-                this.account=item
+                this.quote= {
+                    url: item.quote_url,
+                    datetime: item.datetime,
+                    quote: item.quote,
+                    products: item.product_url,
+                }
+                this.quote_mode="D"
                 this.key=this.key+1
-                this.account_deleting=true
-                this.dialog=true
+                this.dialog_quotescu=true
             },
             update_table(){
                 axios.get(`${this.$store.state.apiroot}/currencies/`, this.myheaders())
                 .then((response) => {
                     this.items=response.data
-                    console.log(this.items)
                 }, (error) => {
                     this.parseResponseError(error)
                 });
--- src/components/QuotesCU.vue
+++ src/components/QuotesCU.vue
@@ -4,10 +4,10 @@
             <MyMenuInline :items="menuinline_items" :context="this"></MyMenuInline>
         </h1>           
         <v-card class="pa-8 mt-2">
-            <v-form ref="form" v-model="form_valid" lazy-validation>                    <AutocompleteProducts v-model="product" :rules="RulesSelection(true)"  />
-                <AutocompleteProducts v-model="newquote.products" :rules="RulesSelection(true)"  />
-                <MyDateTimePicker v-model="newquote.datetime" :label="$t('Set quote date and time')"></MyDateTimePicker>
-                <v-text-field dense v-model.number="newquote.quote"  :label="$t('Set quote')" :placeholder="$t('Set quote')" :rules="RulesFloatGEZ(12,true,product_object.decimals)" counter="12" autofocus/>
+            <v-form ref="form" v-model="form_valid" lazy-validation>
+                <AutocompleteProducts v-model="new_quote.products" :rules="RulesSelection(true)"  />
+                <MyDateTimePicker v-model="new_quote.datetime" :label="$t('Set quote date and time')"></MyDateTimePicker>
+                <v-text-field dense v-model.number="new_quote.quote"  :label="$t('Set quote')" :placeholder="$t('Set quote')" :rules="RulesFloatGEZ(12,true,product_object.decimals)" counter="12" autofocus/>
             </v-form>
             <v-card-actions>
                 <v-spacer></v-spacer>
@@ -30,14 +30,16 @@
             AutocompleteProducts,
         },
         props: {
-            // An account object
-            quote: {
-                required: true // Null to create, io object to update
+            quote: { //A quote object
+                required: true 
             },
+            mode: {
+                requited: true
+            }
         },
         computed:{
             product_object: function(){
-                return this.$store.getters.getObjectByUrl("products",this.newquote.products)
+                return this.$store.getters.getObjectByUrl("products",this.new_quote.products)
             },
             product_stockmarket: function(){
                 return this.$store.getters.getObjectByUrl("stockmarkets",this.product_object.stockmarkets)
@@ -46,7 +48,7 @@
         data(){ 
             return {
                 form_valid:false,
-                newquote: null,
+                new_quote: null,
                 editing:false,
                 menuinline_items: [
                     {
@@ -56,28 +58,28 @@
                                 name: this.$t("Set the time at the start of the futures stock market"),
                                 icon: "mdi-calendar",
                                 code: function(this_){
-                                   this_.newquote.datetime=moment(`${this_.newquote.datetime.slice(0,10)}T${this_.product_stockmarket.starts_futures}`).toISOString()
+                                   this_.new_quote.datetime=moment(`${this_.new_quote.datetime.slice(0,10)}T${this_.product_stockmarket.starts_futures}`).toISOString()
                                 },
                             },
                             {
                                 name: this.$t("Set the time at the start of the stock market"),
                                 icon: "mdi-calendar ",
                                 code: function(this_){
-                                   this_.newquote.datetime=moment(`${this_.newquote.datetime.slice(0,10)}T${this_.product_stockmarket.starts}`).toISOString()
+                                   this_.new_quote.datetime=moment(`${this_.new_quote.datetime.slice(0,10)}T${this_.product_stockmarket.starts}`).toISOString()
                                 },
                             },
                             {
                                 name: this.$t("Set the time at the close of the stock market"),
                                 icon: "mdi-calendar ",
                                 code: function(this_){
-                                   this_.newquote.datetime=moment(`${this_.newquote.datetime.slice(0,10)}T${this_.product_stockmarket.closes}`).toISOString()
+                                   this_.new_quote.datetime=moment(`${this_.new_quote.datetime.slice(0,10)}T${this_.product_stockmarket.closes}`).toISOString()
                                 },
                             },
                             {
                                 name: this.$t("Set the time at the close of the futures stock market"),
                                 icon: "mdi-calendar ",
                                 code: function(this_){
-                                   this_.newquote.datetime=moment(`${this_.newquote.datetime.slice(0,10)}T${this_.product_stockmarket.closes_futures}`).toISOString()
+                                   this_.new_quote.datetime=moment(`${this_.new_quote.datetime.slice(0,10)}T${this_.product_stockmarket.closes_futures}`).toISOString()
                                 },
                             },
                         ]
@@ -88,31 +90,38 @@
         methods: {
             empty_quote,
             title(){
-                if (this.editing){
-                    return this.$t("Updating quote")
-                } else {
-                    return this.$t("Creating a new quote")
-                }
+                if (this.mode=="C") return this.$t("Add a quote")
+                if (this.mode=="U") return this.$t("Update quote")
+                if (this.mode=="D") return this.$t("Delete quote")
             },
             button(){
-                if (this.editing){
-                    return this.$t("Update")
-                } else {
-                    return this.$t("Add")
-                }
+                if (this.mode=="C") return this.$t("Add")
+                if (this.mode=="U") return this.$t("Update")
+                if (this.mode=="D") return this.$t("Delete")
             },
             accept(){
-                if (this.editing==true){
-                    axios.put(this.newquote.url, this.newquote,  this.myheaders())
+                if (this.mode=="U"){
+                    axios.put(this.new_quote.url, this.new_quote,  this.myheaders())
                     .then(() => {
                             this.$emit("cruded")
                     }, (error) => {
                         this.parseResponseError(error)
                     })
-                } else{
-                    axios.post(`${this.$store.state.apiroot}/api/quotes/`, this.newquote,  this.myheaders())
+                } else if (this.mode=="C") {
+                    axios.post(`${this.$store.state.apiroot}/api/quotes/`, this.new_quote,  this.myheaders())
                     .then(() => {
                             this.$emit("cruded")
+                    }, (error) => {
+                        this.parseResponseError(error)
+                    })
+                } else if (this.mode=="D") {
+                    var r = confirm(this.$t("Do you want to delete this quote?"))
+                    if(r == false) {
+                        return
+                    } 
+                    axios.delete(this.new_quote.url, this.myheaders())
+                    .then(() => {
+                        this.$emit("cruded")
                     }, (error) => {
                         this.parseResponseError(error)
                     })
@@ -120,12 +129,7 @@
             },
         },
         created(){
-            if ( this.quote.url!=null){ // EDITING TIENE IO URL
-                this.editing=true
-            } else { // NEW IO BUT SETTING VALUES WITH URL=null
-                this.editing=false
-            }
-            this.newquote=Object.assign({},this.quote)
+            this.new_quote=Object.assign({},this.quote)
         }
     }
 </script>
