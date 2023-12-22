fd26cc617ce5e48d57885460b09d896de0508727
turulomio <turulomio@yahoo.es>
2023-12-22 06:50:12 +0100

    Improved translation system and npm update

--- vue-i18n-extract.config.js
+++ vue-i18n-extract.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "vueFiles": "./src/**/*.?(js|vue)",
-  "languageFiles": "./src/locales/*.?(json|yaml|yml|js)",
-  "exclude": [],
-  "output": false,
-  "add": true,
-  "remove": true,
-  "ci": false,
-  "separator": "###",
-  "noEmptyTranslation": ""
+  vueFiles: "./src/**/*.?(js|vue)",
+  languageFiles: "./src/locales/*.?(json|yaml|yml|js)",
+  exclude: [],
+  output: false,
+  add: true,
+  remove: true,
+  ci: false,
+  separator: "###",
+  noEmptyTranslation: "*",
+  missingTranslationString: "MISSING"
 }
