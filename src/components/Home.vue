1b5370fe05b038c71fcfb06b7ab31a42d8e5a138
turulomio <turulomio@yahoo.es>
2023-12-31 07:33:48 +0100

    First script setup in Home.vue works

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,20 +4,20 @@
         <h2>{{ `${useStore().version} (${useStore().versiondate.toISOString().slice(0,10)})` }}</h2>
         <v-img :src="imgUrl" height="200px" contain ></v-img>
         <v-alert density="compact" class="mx-15 px-10 mb-2" outlined type="warning" v-if="time_message.length>0"> {{time_message}}</v-alert>   
-        <v-alert class="mx-15 px-10 mb-2" type="error" :key="i" variant="outlined" v-if="alerts?.orders_expired.length>0"> 
+        <v-alert class="mx-15 px-10 mb-2" type="error" :key="i" variant="outlined" v-if="alerts.orders_expired?.length>0"> 
             <p>{{f($t("Orders expired in last [0] days :"), [alerts.expired_days])}}</p>
             <ul>    
                 <li v-for="(order,i) in alerts.orders_expired" :key="i">      - {{ order.expiration }}. {{ useStore().investments.get(order.investments).fullname}}</li>
             </ul>
         </v-alert>
-        <v-alert class="mx-15 px-10 mb-2" type="error" :key="i" variant="outlined" v-if="alerts?.investments_inactive_with_balance.length>0"> 
+        <v-alert class="mx-15 px-10 mb-2" type="error" :key="i" variant="outlined" v-if="alerts.investments_inactive_with_balance?.length>0"> 
             <p>{{$t("Inactive investments with balance:")}}</p>
             <ul>    
                 <li v-for="(investment,i) in alerts.investments_inactive_with_balance" :key="i">      - {{ 
                     useStore().investments.get(hyperlinked_url("investments",investment.data.investments_id)).fullname }}. {{ currency_generic_html(investment.total_io_current.balance_investment,investment.data.currency_product)}}</li>
             </ul>
         </v-alert>    
-        <v-alert class="mx-15 px-10 mb-2" type="error" :key="i" variant="outlined" v-if="alerts?.accounts_inactive_with_balance.length>0"> 
+        <v-alert class="mx-15 px-10 mb-2" type="error" :key="i" variant="outlined" v-if="alerts.accounts_inactive_with_balance?.length>0"> 
             <p>{{$t("Inactive accounts with balance:")}}</p>
             <ul>    
                 <li v-for="(account,i) in alerts.accounts_inactive_with_balance" :key="i">      -  {{ 
@@ -33,20 +33,20 @@ import axios from 'axios'
 import { useStore } from "@/store"
 import imgUrl from '@/assets/moneymoney.png'
 import {f} from 'vuetify_rules'
-import { ref,reactive } from 'vue'
+import { ref,reactive, onMounted } from 'vue'
 import { parseResponseError,myheaders } from '@/functions'
 
 
 const diff_time=ref()
-const alerts
+const alerts=reactive({})
 
 function get_alerts(){
     if (!useStore().logged) return
     axios.get(`${useStore().apiroot}/alerts/`, myheaders())
     .then((response) => {
-        alerts=reactive(response.data)
+        alerts.value=response.data
         let local=new Date()
-        let server= new Date(alerts.server_time)
+        let server= new Date(alerts.value.server_time)
         diff_time.value=local-server
 
         console.log(`Local time: ${local.toISOString()}`)
@@ -61,6 +61,9 @@ function time_message(){
     return (this.diff_time.value>=Math.abs(1000)) ? f(this.$t("There is a time difference between the browser and the server of [0] ms. Please contact server administrator."), [this.diff_time]) : ""
 }
 
+onMounted(()=>{
+    get_alerts()
+    
+}); // Automatically fetch data when the component mounts
 
-get_alerts()
 </script>
