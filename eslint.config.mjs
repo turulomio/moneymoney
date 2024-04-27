cc42cd0fb598c561388ec2818791d6cc56683bd8
turulomio <turulomio@yahoo.es>
2024-04-27 11:13:58 +0200

    Addapted eslint to new format (#376)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,16 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "vue/no-v-model-argument": "warn",
+      "vue/valid-v-slot": "off",
+      "js/no-unused-expressions": "warn",
+      "vue/no-unused-vars": "warn",
+
+    }
+  }
+
 ];
+
