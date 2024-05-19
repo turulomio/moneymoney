6b8aa643d28bff7f1b294073589dde9b343b069d
turulomio <turulomio@yahoo.es>
2024-05-19 21:35:56 +0200

    Trying investment chart evolution

--- cypress/e2e/Investments.cy.js
+++ cypress/e2e/Investments.cy.js
@@ -1,15 +1,23 @@
-import { login_test_User, add_investment_from_Home } from "./commons"
+import { login_test_User, add_investmentoperation_from_Home } from "./commons"
 describe('e2e Investments', () => {
   it('Investments', () => {    
 
     login_test_User(cy)
 
 
-    // Add investments
-    add_investment_from_Home(cy) // Is a promise
+    // Add investments and quote and investments operations
+    add_investmentoperation_from_Home(cy) // Is a promise
 
-    // cy.getDataTest('AccountsoperationsSearch_Search').type("comment")
-    // cy.getDataTest('AccountsoperationsSearch_Button').click()
+    // Change investment active status twice
+    cy.getDataTest('MyMenuInline_Button').last().click()
+    cy.getDataTest('MyMenuInline_Header0_Item1').last().click()
+    cy.getDataTest('MyMenuInline_Button').last().click()
+    cy.getDataTest('MyMenuInline_Header0_Item1').last().click()
+
+    // Evolution chart
+    cy.getDataTest('MyMenuInline_Button').last().click()
+    cy.getDataTest('MyMenuInline_Header0_Item2').last().click()
+    cy.getDataTest('ChartInvestmentsoperationsEvolution_ButtonClose').click()
     
   })
 
