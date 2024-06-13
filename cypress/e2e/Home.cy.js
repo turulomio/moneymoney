30949a53fbd43eac0564c4bd99d2f53e23e821d8
turulomio <turulomio@yahoo.es>
2024-06-13 08:39:22 +0200

    Fixing expired orders alert (#400)
    * Test expired orders
    
    * Trying accounts

--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -1,9 +1,58 @@
-import { login_test_User } from "./commons"
+import { 
+  login_test_User,
+  add_investment_from_Home,
+  add_account_from_AccountsList,
+  add_accountoperation_from_AccountsView,
+} from "./commons"
+import {yesterday_in_isostring} from "../../src/functions.js"
 describe('e2e Home', () => {
   it('Home', () => {    
 
     login_test_User(cy)
-  })
+    cy.wait(300)
 
+
+
+
+    // Test inactive account with balance
+    cy.getDataTest('LateralIcon').click()
+    cy.getDataTest('LateralAccounts').click()
+    add_account_from_AccountsList(cy)
+    
+    cy.get("@waitAccountId").then((account_id) =>{
+      //Add ao to set a non zero balance
+      console.log(`AccountsList_Table_Row${account_id}`)
+      cy.wait(300)
+      cy.getDataTest(`AccountsList_Table_Row${account_id}`).click()
+      add_accountoperation_from_AccountsView(cy)
+      cy.getDataTest("AccountsView").type("{esc}")
+
+      //Set inactive
+      cy.getDataTest(`AccountsList_Table_ButtonUpdate${account_id}`).click()
+      cy.get('[id^="checkbox"]')[3].click()
+      cy.getDataTest('AccountsCU_Button').click()
+
+    })
+
+
+    // Test expired order
+    add_investment_from_Home(cy)
+    cy.getDataTest('LateralIcon').click()
+    cy.getDataTest('LateralOrders').click()
+    cy.getDataTest('MyMenuInline_Button').last().click()
+    cy.getDataTest('MyMenuInline_Header0_Item0').click()
+    cy.getDataTest('OrderCU_Expiration').click()
+    cy.get(`[data-v-date="${yesterday_in_isostring()}"] > .v-btn > .v-btn__content`).click()
+    cy.getDataTest('OrderCU_Investments').type("New{downArrow}{enter}")
+    cy.getDataTest('OrderCU_Button').click()
+    cy.getDataTest('OrderCU_ButtonCloseMessage').click()
+    cy.getDataTest('LateralHome').click()
+
+
+
+
+  })
   
+
+
 })
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
             <v-alert class="mx-15 px-10 mb-2" type="error" :key="i" variant="outlined" v-if="alerts.orders_expired.length>0"> 
                 <p>{{f($t("Orders expired in last [0] days :"), [alerts.expired_days])}}</p>
                 <ul>    
-                    <li v-for="(order,i) in alerts.orders_expired" :key="i">      - {{ order.expiration }}. {{ useStore().investments.get(order.investments).fullname}}</li>
+                    <li v-for="(order,i) in alerts.orders_expired" :key="i">      - {{ order.expiration }}. {{ order.investmentsname}}</li>
                 </ul>
             </v-alert>
             <v-alert class="mx-15 px-10 mb-2" type="error" :key="i" variant="outlined" v-if="alerts.investments_inactive_with_balance.length>0"> 
