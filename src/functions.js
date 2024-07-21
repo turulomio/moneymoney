434d9d934b46a8ccc96b95aedc5b8b0333f5126f
turulomio <turulomio@yahoo.es>
2024-07-21 22:27:01 +0200

    Removed function

--- src/functions.js
+++ src/functions.js
@@ -375,23 +375,3 @@ export function amount_to_invest( invested ){
 
 }
 
-
-export const convertImageToDataURL = (url) => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.crossOrigin = 'Anonymous';
-      img.onload = () => {
-        const canvas = document.createElement('canvas');
-        canvas.width = img.width;
-        canvas.height = img.height;
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0);
-        const dataURL = canvas.toDataURL('image/png');
-        resolve(dataURL);
-      };
-      img.onerror = (err) => {
-        reject(err);
-      };
-      img.src = url;
-    });
-  };
