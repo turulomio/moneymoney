678b80c21691143d4109226fc1f38ed30e9d5bc5
turulomio <turulomio@yahoo.es>
2024-07-23 09:37:59 +0200

    Trying to fish vue-i18n-extract

--- vue-i18n-extract.config.js
+++ vue-i18n-extract.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  vueFiles: "./src/**/*.?(js|vue)",
-  languageFiles: "./src/locales/*.?(json|yaml|yml|js)",
-  exclude: [],
-  output: false,
-  add: true,
-  remove: true,
-  ci: false,
-  separator: "###",
-  noEmptyTranslation: "*",
-  missingTranslationString: "MISSING"
+  "vueFiles": "./src/**/*.?(js|vue)",
+  "languageFiles": "./lang/**/*.?(json|yaml|yml|js)",
+  "exclude": [],
+  "output": false,
+  "add": true,
+  "remove": true,
+  "ci": false,
+  "separator": "###",
+  "noEmptyTranslation": "*",
+  "missingTranslationString": "MISSING"
 }
