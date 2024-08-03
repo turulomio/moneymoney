13f3d357370afcefcdb5ba257e5cb8733582132f
turulomio <turulomio@yahoo.es>
2024-08-03 17:48:13 +0200

    Trying

--- cypress/e2e/AssetsReport.cy.js
+++ cypress/e2e/AssetsReport.cy.js
@@ -10,6 +10,7 @@ describe('e2e Assets Report', () => {
     cy.getDataTest('Settings_ButtonSave').click()
 
     cy.getDataTest('LateralAssetsReport').click()
+    cy.getDataTest('AssetsReport_ButtonGenerate', { timeout: 100000 }).should("be.enabled")
     cy.getDataTest('AssetsReport_ButtonGenerate').click()
     cy.getDataTest('AssetsReport_ButtonGenerate', { timeout: 100000 }).should("be.enabled")
   })
