5bc665b0450641c2bff9b29ffd2234e3023adcb3
turulomio <turulomio@yahoo.es>
2025-05-14 19:14:03 +0200

    moneymoney-1.3.0 (#474)
    * moneymoney-1.3.0
    
    * Improved release procedure

--- src/scripts/release.js
+++ src/scripts/release.js
@@ -1,17 +1,17 @@
     console.log(`Release procedure:
+    - Create issue and a branch associated to that issue, and paste code
     - Change version in package.json
     - Change version and versiondate in store.js
     - npm run release
     - Create a new branch: git checkout -b moneymoney-${process.env.npm_package_version}
-    - Edit Changelog in README.md
     - npm run translate
     - Edit src/locales/* if necesary
     - npm run cypress_console
     - git commit -am moneymoney-${process.env.npm_package_version}
-    - git push --set-upstream origin moneymoney-${process.env.npm_package_version}
-    - Merge Pull request in github
+    - git push 
+    - Create Pull request in github checking it has no problem github actions
     - New github release ${process.env.npm_package_version}
+    - Set changelog detailed
     - git checkout main
-    - Return to main branch: git checkout main
-    - Update main branch: git pull
+    - git pull
 `)
