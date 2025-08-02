4a35adba83c64714d7559c090a4e2f16628003d8
turulomio <turulomio@yahoo.es>
2025-08-02 11:55:21 +0200

    Added npm script and changed viewport

--- playwright.config.js
+++ playwright.config.js
@@ -32,6 +32,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     testIdAttribute: 'data-test', // Change to 'data-qa' or 'data-custom-attribute' as needed
+    viewport: { width: 1920, height: 1080 },
   },
 
   /* Configure projects for major browsers */
