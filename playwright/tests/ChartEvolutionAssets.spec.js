fde28f23ba69746f66827aad1906ef4a4f3b5c64
turulomio <turulomio@yahoo.es>
2025-08-04 10:18:08 +0200

    Migrated ChartEvolutionAssets

---
+++ playwright/tests/ChartEvolutionAssets.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from './fixtures.js';
+
+test('Accounts transfers', async ({ page }) => {
+    // 1. Create account to transfer to
+    await page.getByTestId('LateralIcon').click();
+    await page.getByTestId('LateralReports').click();
+    await page.getByTestId('LateralReportsEvolutionChart').click();
+    await expect(page.getByTestId('ChartEvolutionAssets_From')).toBeEnabled()
+});
