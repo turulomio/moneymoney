fc72cc78e1c2fd96661d1544f17b5b704ba6c169
turulomio <turulomio@yahoo.es>
2025-10-11 11:11:29 +0200

    npm update 0 (#522)

--- tests/playwright/StrategiesFastOperations.spec.js
+++ tests/playwright/StrategiesFastOperations.spec.js
@@ -50,7 +50,7 @@ test('Home and its alerts', async ({ page }) => {
 
 
     // Creates an investment and an investment operation
-    const investments_id=await investment_add_from_InvestmentsList(page, "Test investment", "LYXOR IBEX DOBLE APALANCADO (Madrid Stock Exchange)
+    const investments_id=await investment_add_from_InvestmentsList(page, "Test investment", "LYXOR IBEX DOBLE APALANCADO (Madrid Stock Exchange)");
     await expect(page.getByTestId(`Investments_Table_Row${investments_id}`)).toBeVisible();
 
 
