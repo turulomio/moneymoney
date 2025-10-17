8c8fb6b62af573608485a5da9426196cf47a4ea2
turulomio <turulomio@yahoo.es>
2025-10-17 14:13:05 +0200

    EstimationDPS

--- src/components/TableEstimationsDPS.vue
+++ src/components/TableEstimationsDPS.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <v-data-table density="compact" :headers="table_headers()" :items="items" class="elevation-1" :sort-by="[{key:'year',order:'asc'}]" fixed-header :height="$attrs.height"    :items-per-page="10000" >
+        <v-data-table ref="table" density="compact" :headers="table_headers" :items="items" class="elevation-1" :sort-by="[{key:'year',order:'asc'}]" fixed-header :height="$attrs.height" :items-per-page="10000" >
 
             <template #item.estimation="{item}">
                 <div v-html="currency(item.estimation)"></div>
@@ -17,86 +17,88 @@
         <!-- DIVIDEND CU-->
         <v-dialog v-model="dialog_estimation_dps" width="35%">
             <v-card class="pa-3">
-                <EstimationsDpsCU :estimation="estimation_dps" :mode="estimation_dps_mode" :key="key"  @cruded="on_EstimationsDpsCU_cruded()" />
+                <EstimationsDpsCU :estimation="estimation_dps" :mode="estimation_dps_mode" :key="key"  @cruded="on_EstimationsDpsCU_cruded" />
             </v-card>
         </v-dialog>
     </div>
 </template>
-<script>    
+<script setup>
+    import { ref, computed, nextTick } from 'vue'
     import axios from 'axios'
     import { useStore } from "@/store"
     import EstimationsDpsCU from './EstimationsDpsCU.vue'
     import { parseResponseError, myheaders, currency_html } from '@/functions'
-    export default {
-        components:{
-            EstimationsDpsCU,
+    import { useI18n } from 'vue-i18n'
+
+    const props = defineProps({
+        product: {
+            required: true
         },
-        props: {
-            product: {
-                required: true
-            },
-        },
-        data: function(){
-            return {
-                dialog_estimation_dps:false,
-                estimation_dps: null,
-                estimation_dps_mode: null,
-                items:[],
-                key: 0,
-            }
-        },
-        methods: {
-            useStore,
-            parseResponseError,
-            myheaders,
-            currency_html,
-            currency(value){
-                return this.currency_html(value, this.product.currency)
-            },
-            editEstimationDPS(item){
-                this.estimation_dps=item
-                this.estimation_dps_mode="U"
-                this.dialog_estimation_dps=true
-                this.key=this.key+1
-            },
-            deleteEstimationDPS(item){
-                this.estimation_dps=item
-                this.estimation_dps_mode="D"
-                this.dialog_estimation_dps=true
-                this.key=this.key+1
-            },
-            table_headers(){
-                var r= [
-                    { title: this.$t('Year'), key: 'year', sortable: true },
-                    { title: this.$t('DPS estimation'), key: 'estimation', sortable: true,align:'end' },
-                    { title: this.$t('Estimation date'), key: 'date_estimation', sortable: false},
-                    { title: this.$t('Source'), key: 'source', sortable: false,},
-                    { title: this.$t('Manual'), key: 'manual', sortable: true },
-                    { title: this.$t('Actions'), key: 'actions', sortable: false },
-                ]
-                return r
-            },
-            gotoLastRow(){
-                //this.$vuetify.goTo(this.$refs[this.items.length-1], { container:  this.$refs[this.$vnode.tag].$el.childNodes[0] }) 
-            },
-            on_EstimationsDpsCU_cruded(){
-                this.dialog_estimation_dps=false
-                this.refresh()
-            },
-            refresh(){
-                this.loading=true
-                axios.get(`${this.useStore().apiroot}/api/estimationsdps/?product=${this.product.url}`, this.myheaders())
-                .then((response) => {
-                    this.items=response.data
-                    this.key=this.key+1
-                    this.loading=false
-                }, (error) => {
-                    this.parseResponseError(error)
-                });
-            }
-        },
-        created(){
-            this.refresh()
+    })
+
+    const store = useStore()
+    const { t } = useI18n()
+
+    const table = ref(null)
+    const dialog_estimation_dps = ref(false)
+    const estimation_dps = ref(null)
+    const estimation_dps_mode = ref(null)
+    const items = ref([])
+    const key = ref(0)
+    const loading = ref(false)
+
+    function currency(value) {
+        return currency_html(value, props.product.currency)
+    }
+
+    function editEstimationDPS(item) {
+        estimation_dps.value = item
+        estimation_dps_mode.value = "U"
+        dialog_estimation_dps.value = true
+        key.value++
+    }
+
+    function deleteEstimationDPS(item) {
+        estimation_dps.value = item
+        estimation_dps_mode.value = "D"
+        dialog_estimation_dps.value = true
+        key.value++
+    }
+
+    const table_headers = computed(() => [
+        { title: t('Year'), key: 'year', sortable: true },
+        { title: t('DPS estimation'), key: 'estimation', sortable: true, align: 'end' },
+        { title: t('Estimation date'), key: 'date_estimation', sortable: false },
+        { title: t('Source'), key: 'source', sortable: false },
+        { title: t('Manual'), key: 'manual', sortable: true },
+        { title: t('Actions'), key: 'actions', sortable: false },
+    ])
+
+    async function gotoLastRow() {
+        await nextTick()
+        const tableWrapper = table.value?.$el?.querySelector('.v-table__wrapper')
+        if (tableWrapper) {
+            tableWrapper.scrollTop = tableWrapper.scrollHeight
         }
     }
+
+    function on_EstimationsDpsCU_cruded() {
+        dialog_estimation_dps.value = false
+        refresh()
+    }
+
+    function refresh() {
+        loading.value = true
+        axios.get(`${store.apiroot}/api/estimationsdps/?product=${props.product.url}`, myheaders())
+            .then((response) => {
+                items.value = response.data
+                key.value++
+                loading.value = false
+                gotoLastRow()
+            }, (error) => {
+                parseResponseError(error)
+            });
+    }
+
+    refresh()
 </script>
