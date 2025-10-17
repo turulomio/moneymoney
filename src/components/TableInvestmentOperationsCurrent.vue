2c8b20607d215814b36ec9a50d70d60d386316e2
turulomio <turulomio@yahoo.es>
2025-10-17 14:17:19 +0200

    TableIOC

--- src/components/TableInvestmentOperationsCurrent.vue
+++ src/components/TableInvestmentOperationsCurrent.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-data-table density="compact" ref="table_oc" :headers="table_headers()" :items="items" class="elevation-1"  :sort-by="[{key:'datetime',order:'asc'}]" fixed-header :height="$attrs.height" :loading="$attrs.loading" :items-per-page="10000" >
+    <v-data-table density="compact" ref="table_oc" :headers="table_headers" :items="items" class="elevation-1"  :sort-by="[{key:'datetime',order:'asc'}]" fixed-header :height="$attrs.height" :loading="$attrs.loading" :items-per-page="10000" >
         <template #item.datetime="{item}">
             <div>{{ localtime(item.datetime)}}</div>
         </template>   
@@ -98,146 +98,134 @@
                 <template #bottom ></template>   
     </v-data-table>   
 </template>
-<script>    
-    import { listobjects_sum,listobjects_average_ponderated, getMapObjectById, currency_html, percentage_html } from '@/functions'
+<script setup>
+    import { ref, computed, onMounted, nextTick } from 'vue'
+    import { useI18n } from 'vue-i18n'
+    import { listobjects_sum, listobjects_average_ponderated, getMapObjectById, currency_html, percentage_html } from '@/functions'
     import { localtime, f } from 'vuetify_rules'
-    export default {
-        props: {
-            items: {
-                required: true
-            },
-            showinvestment:{// Items must have accounts attribute
-                type: Boolean,
-                required:false,
-                default: false,
-            },
-            output:{ // account, investment, user
-                required:true,
-            },
-            showtotal:{// Items must have currency attribute
-                type: Boolean,
-                required:false,
-                default: false,
-            },
-        },
-        data: function(){
-            return {
-            }
-        },
-        computed:{
-            // To sum shares and average price
-            // Checks if all have the same investments_id == same products_id
-            all_items_have_same_product: function(){           
-                if (this.items.length==0) return false
-                var first_investment_id=this.items[0].investments_id
-                var r=true
-                this.items.forEach(item => {//For Each doesn't allow to return false
-                    if (item.investments_id!=first_investment_id)  {
-                        r=false
-                    }
-                });
-                return r
-            },
-            // To sum amounts
-            all_items_have_same_currency(){
-                if (this.items.length==0) return false
-                var first_currency=this.currency(this.items[0])
-                var r=true
-                this.items.forEach(item => {//For Each doesn't allow to return false
-                    if (this.currency(item)!=first_currency)  {
-                        r=false
-                    }
-                });
-                return r
-            },
-            total_currency: function(){
-                if (this.items.length==0) return "Bad currency"
-                return this.currency(this.items[0])
-            },
-            total_apr(){
-                //Calculate total items apr
-                var sumshares=0
-                var sumsharesxapr=0
 
-                this.items.forEach(o=>{
-                    sumshares=sumshares+o.shares
-                    sumsharesxapr=sumsharesxapr+o.shares*o["percentage_apr_"+this.output]
+    const props = defineProps({
+        items: {
+            required: true
+        },
+        showinvestment:{// Items must have accounts attribute
+            type: Boolean,
+            required:false,
+            default: false,
+        },
+        output:{ // account, investment, user
+            required:true,
+        },
+        showtotal:{// Items must have currency attribute
+            type: Boolean,
+            required:false,
+            default: false,
+        },
+    })
 
-                })
-                return sumsharesxapr/sumshares
-            }
-        },
-        methods: {
-            f,
-            localtime,
-            listobjects_sum,
-            listobjects_average_ponderated,
-            getMapObjectById,
-            currency_html,
-            percentage_html,
-            // Currencies are part of the item
-            currency(item){
-                if (this.output=="account"){
-                    return item.currency_account
-                } else if (this.output=="investment"){
-                    return item.currency_product
-                } else if (this.output=="user"){
-                    return item.currency_user
-                }
-            },
-            table_headers(){
-                var r
-                if (this.output=="account"){
-                    r= [
-                        { title: this.$t('Date and time'), key: 'datetime',sortable: true },
-                        { title: this.$t('Operation'), key: 'operationstypes',sortable: true },
-                        { title: this.$t('Shares'), key: 'shares',sortable: false, align:'end'},
-                        { title: this.$t('Price'), key: 'price_account',sortable: false, align:'end'},
-                        { title: this.$t('Invested'), key: 'invested_account',sortable: false, align:'end'},
-                        { title: this.$t('Balance'), key: 'balance_account',sortable: false, align:'end'},
-                        { title: this.$t('Gross gains'), key: 'gains_gross_account',sortable: false, align:'end'},
-                        { title: this.$t('% annual'), key: 'percentage_annual_account',sortable: false, align:'end'},
-                        { title: this.$t('% APR'), key: 'percentage_apr_account',sortable: false, align:'end'},
-                        { title: this.$t('% Total'), key: 'percentage_total_account',sortable: false, align:'end'},
-                    ]
-                } else if (this.output=="investment"){
-                    r= [
-                        { title: this.$t('Date and time'), key: 'datetime',sortable: true },
-                        { title: this.$t('Operation'), key: 'operationstypes',sortable: true },
-                        { title: this.$t('Shares'), key: 'shares',sortable: false, align:'end'},
-                        { title: this.$t('Price'), key: 'price_investment',sortable: false, align:'end'},
-                        { title: this.$t('Invested'), key: 'invested_investment',sortable: false, align:'end'},
-                        { title: this.$t('Balance'), key: 'balance_investment',sortable: false, align:'end'},
-                        { title: this.$t('Gross gains'), key: 'gains_gross_investment',sortable: false, align:'end'},
-                        { title: this.$t('% annual'), key: 'percentage_annual_investment',sortable: false, align:'end'},
-                        { title: this.$t('% TAE'), key: 'percentage_apr_investment',sortable: false, align:'end'},
-                        { title: this.$t('% Total'), key: 'percentage_total_investment',sortable: false, align:'end'},
-                    ]
-                } else if (this.output=="user"){
-                    r= [
-                        { title: this.$t('Date and time'), key: 'datetime',sortable: true },
-                        { title: this.$t('Operation'), key: 'operationstypes',sortable: true },
-                        { title: this.$t('Shares'), key: 'shares',sortable: false, align:'end'},
-                        { title: this.$t('Price'), key: 'price_user',sortable: false, align:'end'},
-                        { title: this.$t('Invested'), key: 'invested_user',sortable: false, align:'end'},
-                        { title: this.$t('Balance'), key: 'balance_user',sortable: false, align:'end'},
-                        { title: this.$t('Gross gains'), key: 'gains_gross_user',sortable: false, align:'end'},
-                        { title: this.$t('% annual'), key: 'percentage_annual_user',sortable: false, align:'end'},
-                        { title: this.$t('% TAE'), key: 'percentage_apr_user',sortable: false, align:'end'},
-                        { title: this.$t('% Total'), key: 'percentage_total_user',sortable: false, align:'end'},
-                    ]
-                }            
-                if (this.showinvestment==true){
-                    r.splice(1, 0, { title: this.$t('Name'), key: 'investments', sortable: true });
-                }
-                return r
-            },
-            gotoLastRow(){
-                //if (this.$refs.table_oc) this.$vuetify.goTo(1000000, { container:  this.$refs.table_oc.$el.childNodes[0] }) 
-            },
-        },
-        mounted(){
-            this.gotoLastRow()
-        },
+    const { t } = useI18n()
+    const table_oc = ref(null)
+
+    // To sum shares and average price
+    // Checks if all have the same investments_id == same products_id
+    const all_items_have_same_product = computed(() => {
+        if (props.items.length === 0) return false
+        const first_investment_id = props.items[0].investments_id
+        return props.items.every(item => item.investments_id === first_investment_id)
+    })
+
+    // To sum amounts
+    const all_items_have_same_currency = computed(() => {
+        if (props.items.length === 0) return false
+        const first_currency = currency(props.items[0])
+        return props.items.every(item => currency(item) === first_currency)
+    })
+
+    const total_currency = computed(() => {
+        if (props.items.length === 0) return "Bad currency"
+        return currency(props.items[0])
+    })
+
+    const total_apr = computed(() => {
+        //Calculate total items apr
+        let sumshares = 0
+        let sumsharesxapr = 0
+
+        props.items.forEach(o => {
+            sumshares = sumshares + o.shares
+            sumsharesxapr = sumsharesxapr + o.shares * o[`percentage_apr_${props.output}`]
+        })
+        return sumsharesxapr / sumshares
+    })
+
+    // Currencies are part of the item
+    function currency(item){
+        if (props.output === "account"){
+            return item.currency_account
+        } else if (props.output === "investment"){
+            return item.currency_product
+        } else if (props.output === "user"){
+            return item.currency_user
+        }
     }
+
+    const table_headers = computed(() => {
+        let r
+        if (props.output === "account"){
+            r = [
+                { title: t('Date and time'), key: 'datetime',sortable: true },
+                { title: t('Operation'), key: 'operationstypes',sortable: true },
+                { title: t('Shares'), key: 'shares',sortable: false, align:'end'},
+                { title: t('Price'), key: 'price_account',sortable: false, align:'end'},
+                { title: t('Invested'), key: 'invested_account',sortable: false, align:'end'},
+                { title: t('Balance'), key: 'balance_account',sortable: false, align:'end'},
+                { title: t('Gross gains'), key: 'gains_gross_account',sortable: false, align:'end'},
+                { title: t('% annual'), key: 'percentage_annual_account',sortable: false, align:'end'},
+                { title: t('% APR'), key: 'percentage_apr_account',sortable: false, align:'end'},
+                { title: t('% Total'), key: 'percentage_total_account',sortable: false, align:'end'},
+            ]
+        } else if (props.output === "investment"){
+            r = [
+                { title: t('Date and time'), key: 'datetime',sortable: true },
+                { title: t('Operation'), key: 'operationstypes',sortable: true },
+                { title: t('Shares'), key: 'shares',sortable: false, align:'end'},
+                { title: t('Price'), key: 'price_investment',sortable: false, align:'end'},
+                { title: t('Invested'), key: 'invested_investment',sortable: false, align:'end'},
+                { title: t('Balance'), key: 'balance_investment',sortable: false, align:'end'},
+                { title: t('Gross gains'), key: 'gains_gross_investment',sortable: false, align:'end'},
+                { title: t('% annual'), key: 'percentage_annual_investment',sortable: false, align:'end'},
+                { title: t('% TAE'), key: 'percentage_apr_investment',sortable: false, align:'end'},
+                { title: t('% Total'), key: 'percentage_total_investment',sortable: false, align:'end'},
+            ]
+        } else if (props.output === "user"){
+            r = [
+                { title: t('Date and time'), key: 'datetime',sortable: true },
+                { title: t('Operation'), key: 'operationstypes',sortable: true },
+                { title: t('Shares'), key: 'shares',sortable: false, align:'end'},
+                { title: t('Price'), key: 'price_user',sortable: false, align:'end'},
+                { title: t('Invested'), key: 'invested_user',sortable: false, align:'end'},
+                { title: t('Balance'), key: 'balance_user',sortable: false, align:'end'},
+                { title: t('Gross gains'), key: 'gains_gross_user',sortable: false, align:'end'},
+                { title: t('% annual'), key: 'percentage_annual_user',sortable: false, align:'end'},
+                { title: t('% TAE'), key: 'percentage_apr_user',sortable: false, align:'end'},
+                { title: t('% Total'), key: 'percentage_total_user',sortable: false, align:'end'},
+            ]
+        }
+        if (props.showinvestment === true){
+            r.splice(1, 0, { title: t('Name'), key: 'investments', sortable: true });
+        }
+        return r
+    })
+
+    async function gotoLastRow(){
+        await nextTick()
+        const tableWrapper = table_oc.value?.$el?.querySelector('.v-table__wrapper')
+        if (tableWrapper) {
+            tableWrapper.scrollTop = tableWrapper.scrollHeight
+        }
+    }
+
+    onMounted(() => {
+        gotoLastRow()
+    })
 </script>
