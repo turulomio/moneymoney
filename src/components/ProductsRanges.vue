15c3c81369c748dce756aabbdf85e31deb4595e3
turulomio <turulomio@yahoo.es>
2026-03-06 09:37:30 +0100

    Translation added

--- src/components/ProductsRanges.vue
+++ src/components/ProductsRanges.vue
@@ -5,12 +5,12 @@
             
             <v-form ref="form" v-model="form_valid">             
                 <v-row class="d-flex pl-5 pr-5 mx-auto">
-                <AutocompleteProducts data-test="ProductsRanges_Product" class="mr-5" v-model="newpr.product" :rules="RulesSelection(true)"  />
-                <v-autocomplete data-test="ProductsRanges_Investments" class="mr-5" :items="this.getInvestmentsByProduct(newpr.product)" v-model="newpr.investments" :label="$t('Select investments to include')" item-title="fullname" item-value="url" multiple :rules="RulesSelection(true)" chips></v-autocomplete>
+                <AutocompleteProducts data-test="ProductsRanges_Product" class="mr-5" v-model="newpr.product" :rules="RulesSelection(true)" />
+                <v-autocomplete data-test="ProductsRanges_Investments" class="mr-5" :items="getInvestmentsByProduct(newpr.product)" v-model="newpr.investments" :label="$t('Select investments to include')" item-title="fullname" item-value="url" multiple :rules="RulesSelection(true)" chips></v-autocomplete>
 
                 </v-row>
                 <v-row class="pl-5 pr-5">                
-                <v-select data-test="ProductsRanges_RecomendationMethod" class="mr-5" :items="getArrayFromMap(useStore().recomendation_methods)" v-model="newpr.recomendation_methods" :label="$t('Set recomendation method')"  item-title="name" item-value="id" :rules="RulesSelection(true)"></v-select>  
+                <v-select data-test="ProductsRanges_RecomendationMethod" class="mr-5" :items="getArrayFromMap(store.recomendation_methods)" v-model="newpr.recomendation_methods" :label="$t('Set recomendation method')"  item-title="name" item-value="id" :rules="RulesSelection(true)"></v-select>  
 
                 <v-text-field data-test="ProductsRanges_PercentageBetweenRanges" class="mr-5" v-model.number="newpr.percentage_between_ranges"  :label="$t('Set percentage between ranges')" :placeholder="$t('Set percentage between ranges')" :rules="RulesFloat(10,true,6)" counter="10"/>
                 <v-text-field data-test="ProductsRanges_PercentageGains" class="mr-5" v-model.number="newpr.percentage_gains"  :label="$t('Set percentage gains')" :placeholder="$t('Set percentage gains')" :rules="RulesFloat(10,true,6)" counter="10"/>
@@ -18,7 +18,7 @@
 
                 <v-text-field data-test="ProductsRanges_AdditionalRanges" class="mr-5" v-model.number="newpr.additional_ranges"  :label="$t('Additional ranges to show')" :placeholder="$t('Additional ranges to show')" :rules="RulesInteger(2,true)" counter="2"/>
                 <v-checkbox data-test="ProductsRanges_TotalizedOperations" class="mr-5" v-model="newpr.totalized_operations" :label="$t('Show totalized investments operations?')" ></v-checkbox>
-                <v-btn data-test="ProductsRanges_ButtonShow" class="mt-2" color="primary" @click="accept()" :disabled="!form_valid">{{ $t("Show ranges") }}</v-btn>
+                <v-btn data-test="ProductsRanges_ButtonShow" class="mt-2" color="primary" @click="accept" :disabled="!form_valid">{{ $t("Show ranges") }}</v-btn>
 
                 </v-row>
             </v-form>
@@ -82,156 +82,136 @@
 </template>
 
 
-<script>    
+<script setup>
+    import { ref, computed, watch } from 'vue'
+    import { useI18n } from 'vue-i18n'
     import {empty_products_ranges, empty_order} from '../empty_objects.js'
     import { useStore } from "@/store"
     import { RulesSelection, RulesInteger, RulesFloat, f } from 'vuetify_rules'
     import axios from 'axios'
-    import AutocompleteProducts from './AutocompleteProducts.vue'
-    import ChartProductsRanges from './ChartProductsRanges.vue'
-    import InvestmentsView from './InvestmentsView.vue'
-    import OrdersList from './OrdersList.vue'
-    import OrdersCU from './OrdersCU.vue'
     import { parseResponseError, currency_string, myheaders, getArrayFromMap, getInvestmentsByProduct } from '@/functions.js'
 
-    export default {
-        components: {
-            ChartProductsRanges,
-            OrdersCU,
-            OrdersList,
-            InvestmentsView,
-            AutocompleteProducts,
-        },
-        props:{
-            pr:{
-                required:false,
-            }
-        },
-        data(){ 
-            return {
-                showchart:false,
-                tab: null,
-                chart: '',                
-                tableHeaders: [
-                    { title: 'Value', key: 'value',sortable: true, width:"7%"},
-                    { title: 'Must Invest', key: 'recomendation_invest',sortable: false, width:"5%"},
-                    { title: 'Investments',  sortable: false, key: 'investments_inside'},
-                    { title: 'Orders',  sortable: false, key: 'orders_inside'},
-                    { title: 'Actions', key: 'actions', sortable: false , width:"7%"},
-                ],   
-                newpr:this.empty_products_ranges(),
-                currentpricelabel:"",
-                prdata:null,
-                form_valid:false,
-                tableData:[],
-                option: {},
-                loading:false,
-
-                //Dialog OrderCU
-                dialog_ordercu:false,
-                order: null,
-                key:0,
-
-                //Dialog orders list
-                dialog_orders_list:false,
-
-                //Dialog investment view
-                dialog_investment_view:false,
-                investment:null,
-            }   
-        },
-        watch: {
-            tab(){
-                this.showchart=false
-                if (this.tab==1) setTimeout(() => {this.showchart=true }, 300)
-            },
-        },
-        computed:{
-            investments_for_orders(){
-                //Show only investments in this product range
-                var r=[]
-                this.pr.investments.forEach(o=>{
-                    r.push(this.useStore().investments.get(o))
-                })
-                return r
-            }
-        },
-        methods:{
-            useStore,
-            RulesSelection,
-            RulesInteger,
-            RulesFloat,
-            f,
-            parseResponseError,
-            currency_string,
-            myheaders,
-            getArrayFromMap,
-            getInvestmentsByProduct,
-            empty_order,
-            empty_products_ranges,
-            accept(){
-                if (this.form_valid!=true) {
-                    this.$refs.form.validate()
-                    return
-                }
-                this.refreshTable()
-            },
-            addOrder(item){
-                this.order=this.empty_order()
-                this.order.price=item.value
-                this.order.current_price=this.prdata.product.last
-                this.order.shares=Math.floor(this.newpr.amount_to_invest/this.order.price)
-                console.log(this.order)
-                this.key=this.key+1
-                this.dialog_ordercu=true
-            },
-            refreshTable(){
-                this.loading=true
-                var headers={...this.myheaders(),params:this.newpr}
-                axios.get(`${this.useStore().apiroot}/products/ranges/`, headers)
-                .then((response) => {
-                    console.log("Products ranges", response.data)
-                    this.prdata=response.data
-                    this.tableData=this.prdata.pr
-                    this.currentpricelabel= f(this.$t("Current price: [0]"), [this.currency_string(this.prdata.product.last, this.prdata.product.currency)]) + ". "
-                    Object.keys(this.prdata.dataframe[0]).filter(key => !["date", "open", "high", "low", "close", "products_id"].includes(key)).forEach(indicator=> {
-                        this.currentpricelabel+= ` ${indicator}: ${this.currency_string(this.prdata.dataframe[this.prdata.dataframe.length-1][indicator], this.prdata.product.currency)}`
-                    })
-                    this.loading=false
-                }, (error) => {
-                    this.parseResponseError(error)
-                });
-            },
-            showLimits(item){
-                let s=f(this.$t("Range center: [0]"), [this.currency_string(item.value, this.prdata.product.currency)])
-                alert(`${s}\n${item.limits}`)
-            },
-            on_OrdersCU_cruded(){
-                this.dialog_ordercu=false
-                this.refreshTable()
-            },
-            on_orders_inside_click(){
-                this.dialog_orders_list=true
-            },
-            on_dialog_orders_list_click_outside(){
-                this.dialog_orders_list=false
-                this.refreshTable()
-            },
-            on_investments_inside_click(inv){
-                this.investment=inv
-                this.dialog_investment_view=true
-            },
-            on_dialog_investment_view_click_outside(){
-                this.dialog_orders_list=false
-                this.refreshTable()
-            },
-        
-        },
-        created(){
-            if (this.pr!=null){
-                this.newpr=Object.assign({},this.pr)
-                this.refreshTable()
-            }
+    const props = defineProps({
+        pr:{
+            required:false,
         }
+    })
+
+    const store = useStore()
+    const { t } = useI18n()
+    const form = ref(null)
+
+    const showchart = ref(false)
+    const tab = ref(null)
+    const tableHeaders = ref([
+        { title: t('Value'), key: 'value',sortable: true, width:"7%"},
+        { title: t('Must Invest'), key: 'recomendation_invest',sortable: false, width:"5%"},
+        { title: t('Investments'),  sortable: false, key: 'investments_inside'},
+        { title: t('Orders'),  sortable: false, key: 'orders_inside'},
+        { title: t('Actions'), key: 'actions', sortable: false , width:"7%"},
+    ])
+    const newpr = ref(empty_products_ranges())
+    const currentpricelabel = ref("")
+    const prdata = ref(null)
+    const form_valid = ref(false)
+    const tableData = ref([])
+    const loading = ref(false)
+
+    //Dialog OrderCU
+    const dialog_ordercu = ref(false)
+    const order = ref(null)
+    const key = ref(0)
+
+    //Dialog orders list
+    const dialog_orders_list = ref(false)
+
+    //Dialog investment view
+    const dialog_investment_view = ref(false)
+    const investment = ref(null)
+
+    watch(tab, () => {
+        showchart.value = false
+        if (tab.value == 1) setTimeout(() => { showchart.value = true }, 300)
+    })
+
+    const investments_for_orders = computed(() => {
+        //Show only investments in this product range
+        const r=[]
+        if (newpr.value.investments){
+            newpr.value.investments.forEach(o=>{
+                r.push(store.investments.get(o))
+            })
+        }
+        return r
+    })
+
+    function accept(){
+        if (form_valid.value!=true) {
+            form.value.validate()
+            return
+        }
+        refreshTable()
+    }
+
+    function addOrder(item){
+        order.value=empty_order()
+        order.value.price=item.value
+        order.value.current_price=prdata.value.product.last
+        order.value.shares=Math.floor(newpr.value.amount_to_invest/order.value.price)
+        console.log(order.value)
+        key.value=key.value+1
+        dialog_ordercu.value=true
+    }
+
+    function refreshTable(){
+        loading.value=true
+        var headers={...myheaders(),params:newpr.value}
+        axios.get(`${store.apiroot}/products/ranges/`, headers)
+        .then((response) => {
+            console.log("Products ranges", response.data)
+            prdata.value=response.data
+            tableData.value=prdata.value.pr
+            currentpricelabel.value= f(t("Current price: [0]"), [currency_string(prdata.value.product.last, prdata.value.product.currency)]) + ". "
+            Object.keys(prdata.value.dataframe[0]).filter(key => !["date", "open", "high", "low", "close", "products_id"].includes(key)).forEach(indicator=> {
+                currentpricelabel.value+= ` ${indicator}: ${currency_string(prdata.value.dataframe[prdata.value.dataframe.length-1][indicator], prdata.value.product.currency)}`
+            })
+            loading.value=false
+        }, (error) => {
+            parseResponseError(error, t, store)
+        });
+    }
+
+    function showLimits(item){
+        let s=f(t("Range center: [0]"), [currency_string(item.value, prdata.value.product.currency)])
+        alert(`${s}\n${item.limits}`)
+    }
+
+    function on_OrdersCU_cruded(){
+        dialog_ordercu.value=false
+        refreshTable()
+    }
+
+    function on_orders_inside_click(){
+        dialog_orders_list.value=true
+    }
+
+    function on_dialog_orders_list_click_outside(){
+        dialog_orders_list.value=false
+        refreshTable()
+    }
+
+    function on_investments_inside_click(inv){
+        investment.value=inv
+        dialog_investment_view.value=true
+    }
+
+    function on_dialog_investment_view_click_outside(){
+        dialog_investment_view.value=false
+        refreshTable()
+    }
+
+    if (props.pr!=null){
+        newpr.value=Object.assign({},props.pr)
+        refreshTable()
     }
 </script>
