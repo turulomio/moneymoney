c575974a893a6910c7ba6ae4f2e5003b1437bac7
turulomio <turulomio@yahoo.es>
2026-03-22 18:29:44 +0100

    Committing in 1.4.0 by error
    * moneymoney-1.4.0
    
    * Improving
    
    * No se que está mal

--- tests/playwright/commons.js
+++ tests/playwright/commons.js
@@ -53,6 +53,20 @@ export async function accountoperation_add_from_AccountsView(page, concept, amou
   return newId
 }
 
+export async function accountstransfer_from_AccountsView(page, destiny_account_name, amount, commission) {
+  // Assumes the test is already in the AccountsView of the source account
+  await mymenuinline_selection(page, "AccountsView_MyMenuInline", 0, 0)
+  await v_autocomplete_selection_with_role_option(page, 'AccountsTransfer_Destiny', destiny_account_name)
+  await v_text_input_settext(page, 'AccountsTransfer_Amount', amount)
+  await v_text_input_settext(page, 'AccountsTransfer_Commission', commission)
+  const object_promised = promise_to_get_response(page, "/api/accountstransfers/", "POST");
+  await page.getByTestId('AccountsTransfer_Button').click()
+  const r = await object_promised;
+  expect(r).toBeDefined();
+  await expect(page.getByTestId('AccountsTransfer_Button')).toBeHidden()
+  return r
+}
+
 
 
 export async function creditcard_add_from_AccountsView(page, name, deferred){
