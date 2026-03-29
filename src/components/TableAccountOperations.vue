e946c3d240cdc5de83a4a754b4c49d508c80f03c
turulomio <turulomio@yahoo.es>
2026-03-29 08:51:03 +0200

    Removed innecesary defineExpose

--- src/components/TableAccountOperations.vue
+++ src/components/TableAccountOperations.vue
@@ -85,7 +85,7 @@
 </template>
 
 <script setup>
-    import { ref, computed, watch, onMounted, nextTick , defineExpose} from 'vue'
+    import { ref, computed, watch, onMounted, nextTick} from 'vue'
     import axios from 'axios'
     import { useStore } from "@/store"
     import {  empty_account_operation } from '../empty_objects.js'
