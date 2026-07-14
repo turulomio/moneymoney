cfa95d5e70aedb0886613cb98cd7869127d32328
turulomio <turulomio@yahoo.es>
2026-07-14 08:30:05 +0200

    Added change in fixture·

--- tests/playwright/fixtures.js
+++ tests/playwright/fixtures.js
@@ -34,10 +34,7 @@ const test = baseTest.extend({
   await page.getByTestId('BtnLogIn_User').getByRole('textbox').fill("test");
   await page.getByTestId('BtnLogIn_Password').getByRole('textbox').fill("test");
   await page.getByTestId('BtnLogIn_cmd').click();
-//   await page.waitForURL('http://127.0.0.1:8006/moneymoney/home/')
-    
-    // Wait for successful login, e.g., navigation to dashboard
-    await expect(page).toHaveURL('http://127.0.0.1:8006/moneymoney/about/');
+  await page.waitForURL('http://127.0.0.1:8006/moneymoney/home/');
     //await expect(page.getByTestId("LateralAssetsReport")).toBeVisible()
     
     // Use the logged-in page in the test
