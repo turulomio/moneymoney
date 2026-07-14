3d34f45fb1e78dbb22171fa044dcf318b4b751d2
turulomio <turulomio@yahoo.es>
2026-07-14 08:09:32 +0200

    Added playwright automatic server

--- playwright.config.js
+++ playwright.config.js
@@ -42,5 +42,14 @@ export default defineConfig({
       // dependencies: ['setup'],
     },
   ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:8006/moneymoney/',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
 });
+
 
