fa4554abfd22525117e98d39c32e397c38b2c20a
turulomio <turulomio@yahoo.es>
2026-07-14 14:05:18 +0200

    Improving

--- playwright.config.js
+++ playwright.config.js
@@ -50,7 +50,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run preview',
+    command: process.env.CI ? 'npm run preview' : 'npm run dev', //preview no covertura
     url: 'http://127.0.0.1:8006/moneymoney/',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
